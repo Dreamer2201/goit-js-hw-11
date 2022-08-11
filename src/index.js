efaf7b89814524950b4e59b2b5f8dad3ca166abe
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-08-11 09:49:17 +0300

    hide btn load more

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ import {insertCreatedAnimals} from './js/createListAnimals';
 
 export let currentPage = 1;
 
+const BASE_URL = 'https://pixabay.com/api/';
+const myAPIkey = '29146874-e25e04f0bbd5e8c4fffc4a4f6';
+const perPages = 40;
+
+refs.btnLoadMoreEl.classList.add("hide");
+
 refs.formEl.addEventListener('submit', onSubmitForm);
 refs.btnLoadMoreEl.addEventListener('click', onSubmitForm);
 
@@ -21,15 +27,23 @@ function onSubmitForm (event) {
         refs.totalHitsEl.innerHTML = `Hooray! We found totalHits ${data.total} images.`
         console.log(data);
         filterAnimals(data);
+        hidenBtnLoadMore(data);
+        
     })
     .catch(error => console.log(error))
 }
-function checkEvent (event, data) {
+function checkEvent (event) {
     if (event.type === 'submit') {
         clearSearchResults();
-       return currentPage = 1;
-    
-    } currentPage +=1;
+        refs.btnLoadMoreEl.classList.remove("hide");
+       return currentPage = 1;  
+    } currentPage +=1;  
+}
+function hidenBtnLoadMore(data) {
+    if (currentPage === Math.ceil(data.total / perPages)) {
+        refs.btnLoadMoreEl.classList.add("hide");
+        Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    }
 }
 function filterAnimals(animals) {
     if (animals.total === 0) {
@@ -41,15 +55,12 @@ function filterAnimals(animals) {
 function clearSearchResults () {
     refs.galleryEl.innerHTML = "";
 }
-const BASE_URL = 'https://pixabay.com/api/';
-const myAPIkey = '29146874-e25e04f0bbd5e8c4fffc4a4f6';
 
 const fetchPictures = animal => {
-    return fetch(`${BASE_URL}?key=${myAPIkey}&q=${animal}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`)
+    return fetch(`${BASE_URL}?key=${myAPIkey}&q=${animal}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPages}&page=${currentPage}`)
     .then((response) => {
         return response.json();
     })   
 }
 
-
-// new SimpleLightbox('.gallery__item', { captionSelector: 'img', captions: true, captionsData: 'alt', overlayOpacity: 0.5, navText: 	['←','→'], captionPosition: 'bottom', captionDelay: 250 });
+const lightbox = new SimpleLightbox('.gallery__item', { captionSelector: 'img', nav: true, captions: true, captionType: 'alt', captionsData: 'alt', overlayOpacity: 0.5, navText: 	['←','→'], captionPosition: 'bottom', captionDelay: 250 });
