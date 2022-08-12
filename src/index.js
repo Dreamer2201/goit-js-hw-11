705cdef0a50359a6e4e535ca84c5be771e9b51b6
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-08-12 12:17:23 +0300

    try add axios.get

--- src/index.js
+++ src/index.js
@@ -11,37 +11,51 @@ export let currentPage = 1;
 
 refs.btnLoadMoreEl.classList.add("hide");
 
-async function onSubmitForm (event) {
+refs.formEl.addEventListener('submit', onSubmitForm);
+refs.btnLoadMoreEl.addEventListener('click', onSubmitForm);
+
+function onSubmitForm (event) {
     event.preventDefault();
     const animal = refs.inputEl.value;
     checkEvent(event);
+    convertFetchResults(animal);
+    
+}
+async function convertFetchResults (animalName) {
     try {
-        const fetchResult = await fetchPictures(animal);
+        const fetchResult = await fetchPictures(animalName);
         const data = fetchResult.hits;
-        refs.totalHitsEl.innerHTML = `Hooray! We found totalHits ${data.total} images.`;
+        console.log(fetchResult);
         console.log(data);
-        filterAnimals(data);
-        hidenBtnLoadMore(data);
+        // refs.totalHitsEl.innerHTML = `Hooray! We found ${data.length} images.`;
+        // filterAnimals(data);
     } catch (error) {console.log(error)}
 }
-
 function checkEvent (event) {
     if (event.type === 'submit') {
         clearSearchResults();
         return currentPage = 1;  
     } currentPage +=1;
 }
-function filterAnimals(animals) {
-    if (animals.length === 0) {
-        refs.btnLoadMoreEl.classList.add("hide");
+function filterAnimals(data) {
+    if (data.length < 40) {
+        console.log(data.length);
+        hidenBtnLoadMore(data);
         Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        return;
     } else { 
-        insertCreatedAnimals(animals);
+        console.log(data.length);
+        insertCreatedAnimals(data);
         refs.btnLoadMoreEl.classList.remove("hide");
+        smoothScrollToBottomPage();
     }
 }
-
-refs.formEl.addEventListener('submit', onSubmitForm);
-refs.btnLoadMoreEl.addEventListener('click', onSubmitForm);
+function smoothScrollToBottomPage () {
+    const galleryRect = refs.galleryEl.getBoundingClientRect();
+    window.scrollBy({
+        top: galleryRect.height,
+        behavior: "smooth",
+    })
+}
 
 // const lightbox = new SimpleLightbox('.gallery__item', { captionSelector: 'img', nav: true, captions: true, captionType: 'alt', captionsData: 'alt', overlayOpacity: 0.5, navText: 	['←','→'], captionPosition: 'bottom', captionDelay: 250 });
