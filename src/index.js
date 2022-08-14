388a59d711f945b7321261adc70ef758dfa3291f
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-08-14 07:16:26 +0300

    corect modules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import {Notify} from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import {refs} from './js/refs';
-import {filterAnimals} from './js/filterPictures';
-import {clearGalleryList} from './js/clearGalleryList';
+import {insertCreatedAnimals} from './js/createListAnimals';
+import {smoothScrollToBottomPage} from './js/smoothScroll';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const myAPIkey = '29146874-e25e04f0bbd5e8c4fffc4a4f6';
 export const perPages = 40;
-export let currentPage = 1;
+let currentPage = 1;
 
 refs.btnLoadMoreEl.classList.add("hide");
 
@@ -40,5 +41,27 @@ async function convertFetchResults (animalName) {
         refs.totalHitsEl.innerHTML = `Hooray! We found ${fetchResult.total} images.`;
         filterAnimals(data);
     } catch (error) {console.log(error)}
+}
+function filterAnimals(data) {
+    if (data.length > 0 && data.length < 40) {
+        insertCreatedAnimals(data);  
+        refs.btnLoadMoreEl.classList.add("hide");
+        Notify.info("We're sorry, but you've reached the end of search results.");
+        smoothScrollToBottomPage();
+        lightbox.refresh();
+        return;
+    } else if (data.length === 0) {
+        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        return;
+    } else { 
+        insertCreatedAnimals(data);  
+        refs.btnLoadMoreEl.classList.remove("hide");
+        smoothScrollToBottomPage();
+        lightbox.refresh();
+        return;
+    }
+}
+function clearGalleryList () {
+    refs.galleryEl.innerHTML = "";
 }
 
